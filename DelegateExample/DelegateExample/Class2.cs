3db0366d5a634bd86a49cf354d2f38be724c67f9
sumon <sumoniceiu1415@gmail.com>
2024-05-14 18:17:17 +0600

    added delegate example code

---
+++ DelegateExample/DelegateExample/Class2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateExample
+{
+    internal class Class2
+    {
+        public int Multiplication(int x,int y)
+        {
+            return x*y;
+        }
+
+        public delegate double Ddivision(double x,double y);
+
+        public double division(double x,double y)
+        {
+            return x / y;
+
+        }
+    }
+}
---
+++ DelegateExample/DelegateExample/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static DelegateExample.Class2;
+
+namespace DelegateExample
+{
+    internal class Program
+    {
+        public delegate void Dmethod(string m);
+        public delegate int sum(int a, int b);
+
+        public delegate int mul(int a, int b);
+
+        delegate void perform(string text);
+
+
+        static void Main(string[] args)
+        {
+            Dmethod del  = new Dmethod(printm);
+            del("Hello world");
+
+            sum s = new sum(addnum);
+             var result= s(12, 23);
+            Console.WriteLine(result);
+
+            Class2 ob= new Class2();
+           
+            mul Dob = new mul(ob.Multiplication);
+            var re = Dob(12, 34);
+            Console.WriteLine($"multiplication of two number is {re}");
+
+           
+            Ddivision dob2 = new Ddivision(ob.division);
+            var re2 = dob2(12, 3);
+            Console.WriteLine("the division result is "+re2);
+
+            var logic = new perform(printingmethod);
+
+            logic("hello");
+
+            var text = "hellow world";     
+
+            procestext(text, logic);  //using  extra printing  logic
+
+
+
+        }
+
+        static int addnum(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void printm(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        public static void printingmethod(string s)
+        {
+            Console.WriteLine(s);
+
+        }
+        static void procestext(string text,perform perform)
+        {
+            if (!string.IsNullOrEmpty(text))
+                perform(text);
+
+        }
+    }
+}
