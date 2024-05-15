44041d523fd4d733a4356c41848de9492ce44897
sumon <sumoniceiu1415@gmail.com>
2024-05-15 13:55:11 +0600

    added  FirstDemo example code

---
+++ FirstDemo/FirstDemo/Controllers/DashboardController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstDemo.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            
+                return View();
+            
+        }
+        public IActionResult Summary()
+        {
+            return View();
+                
+        }
+    }
+}
