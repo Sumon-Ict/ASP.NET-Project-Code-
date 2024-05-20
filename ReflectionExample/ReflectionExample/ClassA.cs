b4439b49a9151d2890b51f6beb3ab194ca0f1b07
sumon <sumoniceiu1415@gmail.com>
2024-05-20 20:29:45 +0600

    added reflection example

---
+++ ReflectionExample/ReflectionExample/ClassA.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionExample
+{
+    internal class ClassA
+    {
+        public string Name { get; set; }    
+        public string Description { get; set; }
+        private int ID {  get; set; }
+        private DateTime dateTime { get; set; }
+
+        public void mehtod(int id)
+        {
+            this.ID = id;
+        }
+
+        private void methodb(int id)
+        {
+
+        }
+        public ClassA()
+        {
+
+        }
+        public void result()
+        {
+
+        }
+
+
+    }
+}
---
+++ ReflectionExample/ReflectionExample/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var type = typeof(ClassA);
+
+            Console.WriteLine(type.FullName);
+            Console.WriteLine(type.Name);
+            Console.WriteLine("____fields names----------");
+            var fields= type.GetFields();
+
+            foreach(var field in fields)
+            {
+                Console.WriteLine(field.Name);
+            }
+
+            Console.WriteLine("---------methods name---------");
+
+            var methods = type.GetMethods();
+            foreach(var method in methods)
+            {
+             
+                Console.WriteLine(method.Name);
+            }
+
+            Console.WriteLine("----------members names------");
+
+            var members=type.GetMembers();
+
+            foreach(var member in members)
+            {
+                Console.WriteLine(member.Name);
+            }
+
+            Console.WriteLine("-------properties names----------");
+
+            var properties=type.GetProperties();
+
+            foreach(var property in properties)
+            {
+                Console.WriteLine(property.Name);
+            }
+
+            Console.WriteLine("----------constructors name-----------");
+
+            var cosntructors=type.GetConstructors();
+
+            foreach( var c in cosntructors)
+            {
+                Console.WriteLine(c.Name);
+            }
+            var r = type.Assembly.GetName();
+
+            Console.WriteLine(r);
+
+
+
+        }
+    }
+}
